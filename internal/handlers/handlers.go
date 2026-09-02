7e79d68e1ecbfb207f4d7aede6c3adccff69e3da
Daniel J. Manning <daniel@danieljmanningdev.com>
2026-09-02 10:56:17 +0100

    feat: add reusable static page renderer

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,34 @@
 package handlers
 
-import "html/template"
+import (
+	"net/http"
+	"time"
+)
 
 type Handler struct {
 	Templates map[string]*template.Template
+}
+
+type PageData struct {
+	Title       string
+	Description string
+	Year        int
+}
+
+func (h *Handler) renderPage(
+	w http.ResponseWriter,
+	key string,
+	data PageData,
+) {
+	data.Year = time.Now().Year()
+
+	tmpl, ok := h.Templates[key]
+	if !ok {
+		http.Error(w, "Template not found", http.StatusInternalServerError)
+		return
+	}
+
+	if err := tmpl.ExecuteTemplate(w, "base", data); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
