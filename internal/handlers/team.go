0576ee1150dc80cc5654b2b4a076e35598769050
Daniel J. Manning <daniel@danieljmanningdev.com>
2026-09-02 10:57:04 +0100

    feat: add team page handler

--- internal/handlers/team.go
+++ internal/handlers/team.go
@@ -1,1 +1,14 @@
 package handlers
+
+import "net/http"
+
+func (h *Handler) Team(w http.ResponseWriter, r *http.Request) {
+	h.renderPage(
+		w,
+		"team",
+		PageData{
+			Title:       "Team | Salon",
+			Description: "Meet the fictional creative team behind this contemporary salon portfolio concept.",
+		},
+	)
+}
