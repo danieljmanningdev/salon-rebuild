336b0a68ddf8dfe6fc2b74c90139d16a1edbde74
Daniel J. Manning <daniel@danieljmanningdev.com>
2026-09-02 10:57:42 +0100

    feat: register static site routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,6 +10,10 @@ func Routes(h *handlers.Handler) *http.ServeMux {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", h.Home)
+	mux.HandleFunc("/services", h.Services)
+	mux.HandleFunc("/team", h.Team)
+	mux.HandleFunc("/gallery", h.Gallery)
+	mux.HandleFunc("/contact", h.Contact)
 
 	mux.Handle(
 		"/static/",
