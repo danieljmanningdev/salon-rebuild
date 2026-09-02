fc790b38dff678e626443a7e937c3acbde524772
Daniel J. Manning <daniel@danieljmanningdev.com>
2026-09-02 10:56:50 +0100

    feat: add services page handler

--- internal/handlers/services.go
+++ internal/handlers/services.go
@@ -1,1 +1,14 @@
 package handlers
+
+import "net/http"
+
+func (h *Handler) Services(w http.ResponseWriter, r *http.Request) {
+	h.renderPage(
+		w,
+		"services",
+		PageData{
+			Title:       "Services | Salon",
+			Description: "Explore a fictional menu of contemporary hair and beauty services created for this portfolio concept.",
+		},
+	)
+}
